c237f74feacc4c6267d186b8841d45aea832d140
Rivan Main <rivan.farok@gmail.com>
2026-07-27 11:38:24 +0300

    feat: the system nav bar is now hidden

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,11 +1,5 @@
 import { Stack } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
 
 export default function AuthLayout() {
-  return (
-    <>
-      <StatusBar style='auto' />
-      <Stack screenOptions={{ headerShown: false, animation: 'none' }} />
-    </>
-  )
+  return <Stack screenOptions={{ headerShown: false, animation: 'none' }} />
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,16 @@
 import AppProviders from '@/providers/AppProviders'
 import { Stack } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
+
+import SystemBars from '../../components/SystemBars'
+
 export default function RootLayout() {
   return (
-    <>
-      <AppProviders>
-        <StatusBar style='auto' />
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name='(auth)' />
-          <Stack.Screen name='index' />
-        </Stack>
-      </AppProviders>
-    </>
+    <AppProviders>
+      <SystemBars />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='(auth)' />
+        <Stack.Screen name='index' />
+      </Stack>
+    </AppProviders>
   )
 }
